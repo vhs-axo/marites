0eefce7df0973bc68868548b986a475c8d2bef18
vhs-axo <mkpalarpalar@gmail.com>
2024-05-25 15:03:04 +0800

    discard

--- src/App.py
+++ src/App.py
@@ -1,10 +1,17 @@
 from managers.manager import BoardingHouseManager
 from models.entities import *
 from services.service import SessionFactory
+from views.rooms import RoomListWindow
+from controllers.controller import RoomListController
 from datetime import date
 
 def main() -> None:
-   pass
+   x = RoomListWindow()
+   b = BoardingHouseManager(SessionFactory("root", "ax+vh$_jk&kr1").get_session())
+   
+   RoomListController(b, x)
+   
+   x.mainloop()
 
 if __name__ == '__main__':
    main()
