016193f3cf787e608c5136150bfc1c520c55797f
vhs-axo <mkpalarpalar@gmail.com>
2024-05-25 13:31:51 +0800

    roomlist controller

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -1,8 +1,23 @@
 
 from managers.manager import BoardingHouseManager
-from views.main import RoomWindow
+from views.rooms import RoomListWindow, RoomOpenWindow
+from views.forms import RoomForm, TenantForm, LeaseForm, PaymentForm
 
-
-class RoomsController:
-    def __init__(self, manager: BoardingHouseManager, window: RoomWindow) -> None:
-        pass
+class RoomListController:
+    def __init__(self, manager: BoardingHouseManager, window: RoomListWindow) -> None:
+        self.manager = manager
+        self.window = window
+    
+    def load_rooms(self):
+        for room in self.manager.get_all_rooms():
+            self.window.rooms_treeview.insert(
+                "",
+                "end",
+                room.room_number,
+                values=(
+                    f"Room {room.room_number}",
+                    f"{room.tenant_count} / {room.max_capacity}"
+                )
+            )
+    
+    
